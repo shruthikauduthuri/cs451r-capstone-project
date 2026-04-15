6309bf4ccf32a8f0d91d2e99a027dddf2a6e670d
Montana Nicholson <nicholsonmontana@gmail.com>
2026-04-14 23:43:14 -0500

    Created Request/Response Classes

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcfed91e0d1cb2b2d42c47d970eeda7b6a0d2999")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aca5b05ffd0cc1ad3b9525bc60faf2384254e4e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
