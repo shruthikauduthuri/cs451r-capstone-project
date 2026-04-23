d07ebee72b1463949ef86b1b758f10eee3105f9b
Montana Nicholson <nicholsonmontana@gmail.com>
2026-04-22 21:58:58 -0500

    staging

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69cae3f43e8fd10020b98bc7e780fe83aee139a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e76fee00dcef5116c08231c122aa6301b4c79a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
